3ec62e869eb853f022022e5834778c1d9703021e
Mercedes Marighetti <anamercedes.marighetti@bbva.com>
2025-06-22 19:43:44 -0300

    Asistencia is working

--- app/src/main/java/com/example/tsdsapp/ui/asistencia/AsistenciaFragment.kt
+++ app/src/main/java/com/example/tsdsapp/ui/asistencia/AsistenciaFragment.kt
@@ -9,37 +9,34 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tsdsapp.R
 import com.example.tsdsapp.data.MateriaCompleta
 import com.example.tsdsapp.data.MateriasAlumno
 import com.example.tsdsapp.databinding.FragmentAsistenciaBinding
+import com.example.tsdsapp.ui.adapter.AsistenciaAdapter
+import com.example.tsdsapp.ui.adapter.CalificacionesAdapter
 import com.example.tsdsapp.ui.adapter.MateriaAdapter
 
 class AsistenciaFragment : Fragment() {
-    lateinit var recyclerView: RecyclerView
     private var _binding: FragmentAsistenciaBinding? = null
     private val binding get() = _binding!!
-    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentAsistenciaBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        recyclerView = binding.recyclerAsistencia
-        recyclerView.adapter = MateriaAdapter(MateriasAlumno.generarMockData())
-        return root
+    ): View? { val view = inflater.inflate(R.layout.fragment_asistencia, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerAsistencia)
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = AsistenciaAdapter(MateriasAlumno.generarMockData())
+
+
+        return view
     }
 
 override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    sharedPreferences = requireActivity().getSharedPreferences("usuarioLogueado", Context.MODE_PRIVATE)
-
-    // Obtener los datos guardados
-    val nombre = sharedPreferences.getString("nombre_usuario", "Usuario no identificado")
-    val dni = sharedPreferences.getString("dni_usuario", "DNI no disponible")
-
-
 }
     override fun onDestroyView() {
         super.onDestroyView()
